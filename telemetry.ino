222f26981a28bedd31906a3fc6666a8e41b4b978
leo <niels.l.larsen-1@ou.edu>
2025-05-07 11:41:00 -0500

    added starting commit for sake of orginization

---
+++ telemetry.ino
@@ -0,0 +1,10 @@
+void startup()
+{
+    // This function is called at the start of the program.
+    // It can be used to initialize any global variables or perform setup tasks.
+}
+void loop()
+{
+    // This function is called repeatedly in a loop.
+    // It can be used to perform tasks that need to run continuously.
+}
